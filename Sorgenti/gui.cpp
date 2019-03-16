04625474cf0af6ef7740e9249121b97abe29a267
Sala Carlo <info@carlosala.net>
2019-03-16 08:54:43 +0100

    Update gui.cpp Signed-off-by: Sala Carlo <info@carlosala.net>

--- Sorgenti/gui.cpp
+++ Sorgenti/gui.cpp
@@ -23,7 +23,7 @@ MainFrameBase::MainFrameBase( wxWindow* parent, wxWindowID id, const wxString& t
 	wxTextValidator textValidator(wxFILTER_INCLUDE_CHAR_LIST);
 	textValidator.SetIncludes(includeList);
     //SetIcon(wxICON(sample));    
-    this->SetSize(wxSize(dpiz(390), dpiz(340)));
+    this->SetSize(wxSize(dpiz(400), dpiz(355)));
 	this->SetSizeHints( wxDefaultSize, wxDefaultSize );
 	this->SetBackgroundColour( wxColour( 180, 180, 180 ) );
 	
@@ -61,29 +61,29 @@ MainFrameBase::MainFrameBase( wxWindow* parent, wxWindowID id, const wxString& t
 	m_staticText3 = new wxStaticText( m_panel1, wxID_ANY, _("Altezza in cm.:"), wxDefaultPosition, wxSize(dpiz(122), -1), 0 );
 	m_staticText3->Wrap( -1 );
 	m_staticText3->SetFont(wxFontInfo(9).FaceName("Arial"));
-	bSizer4->Add( m_staticText3, 0, wxALL|wxALIGN_LEFT, 5 );
+	bSizer4->Add( m_staticText3, 0, wxALL, 5 );
 	
 	m_textCtrl1 = new wxTextCtrl( m_panel1, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(dpiz(60), -1), 0|wxWANTS_CHARS,textValidator );
 	m_textCtrl1->SetMaxLength( 3 );
 	m_textCtrl1->SetFont(wxFontInfo(9).FaceName("Arial").Bold());
-	bSizer4->Add( m_textCtrl1, 0, wxEXPAND|wxALIGN_LEFT, 5 );
+	bSizer4->Add( m_textCtrl1, 0, wxEXPAND, 5 );
 	
 	m_staticText4 = new wxStaticText( m_panel1, wxID_ANY, _(","), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticText4->Wrap( -1 );
 	m_staticText4->SetFont(wxFontInfo(9).FaceName("Arial"));
-	bSizer4->Add( m_staticText4, 0, wxALL|wxALIGN_CENTER_VERTICAL|wxALIGN_LEFT, 5 );
+	bSizer4->Add( m_staticText4, 0, wxALL, 5 );
 	
 	m_textCtrl2 = new wxTextCtrl( m_panel1, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(dpiz(35), -1), 0|wxWANTS_CHARS,textValidator );
 	m_textCtrl2->SetMaxLength( 2 ); 
 	m_textCtrl2->SetFont(wxFontInfo(9).FaceName("Arial").Bold());	
-	bSizer4->Add( m_textCtrl2, 0, wxEXPAND|wxALIGN_LEFT, 5 );
+	bSizer4->Add( m_textCtrl2, 0, wxEXPAND, 5 );
 	
 	
 	bSizer4->Add( 60, 0, 0, wxEXPAND, 5 );
 	
 	m_radioBtn1 = new wxRadioButton( m_panel1, wxID_ANY, _("Maschio"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_radioBtn1->SetValue( true ); 
-	bSizer4->Add( m_radioBtn1, 0, wxALIGN_CENTER_VERTICAL|wxALL|wxALIGN_LEFT, 5 );
+	bSizer4->Add( m_radioBtn1, 0, wxALL, 5 );
 	
 	
 	bSizer2->Add( bSizer4, 0, wxBOTTOM|wxEXPAND|wxTOP, 5 );
@@ -95,28 +95,28 @@ MainFrameBase::MainFrameBase( wxWindow* parent, wxWindowID id, const wxString& t
 	m_staticText5->Wrap( -1 );
 	m_staticText5->SetFont(wxFontInfo(9).FaceName("Arial"));
 	
-	bSizer6->Add( m_staticText5, 0, wxALL|wxALIGN_LEFT, 5 );
+	bSizer6->Add( m_staticText5, 0, wxALL, 5 );
 	
 	m_textCtrl3 = new wxTextCtrl( m_panel1, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(dpiz(60), -1), 0|wxWANTS_CHARS,textValidator );
 	m_textCtrl3->SetMaxLength( 3 ); 
 	m_textCtrl3->SetFont(wxFontInfo(9).FaceName("Arial").Bold());	
-	bSizer6->Add( m_textCtrl3, 0, wxEXPAND|wxALIGN_LEFT, 5 );
+	bSizer6->Add( m_textCtrl3, 0, wxEXPAND, 5 );
 	
 	m_staticText6 = new wxStaticText( m_panel1, wxID_ANY, _(","), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticText6->Wrap( -1 );
 	m_staticText6->SetFont(wxFontInfo(9).FaceName("Arial"));
-	bSizer6->Add( m_staticText6, 0, wxALL|wxALIGN_CENTER_VERTICAL|wxALIGN_LEFT, 5 );
+	bSizer6->Add( m_staticText6, 0, wxALL, 5 );
 	
 	m_textCtrl4 = new wxTextCtrl( m_panel1, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(dpiz(35), -1), 0|wxWANTS_CHARS,textValidator );
 	m_textCtrl4->SetMaxLength( 2 );
 	m_textCtrl4->SetFont(wxFontInfo(9).FaceName("Arial").Bold());
-	bSizer6->Add( m_textCtrl4, 0, wxEXPAND|wxALIGN_LEFT, 5 );
+	bSizer6->Add( m_textCtrl4, 0, wxEXPAND, 5 );
 	
 	
 	bSizer6->Add( 60, 0, 0, wxEXPAND, 5 );
 	
 	m_radioBtn2 = new wxRadioButton( m_panel1, wxID_ANY, _("Femmina"), wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer6->Add( m_radioBtn2, 0, wxALL|wxALIGN_CENTER_VERTICAL|wxALIGN_LEFT, 5 );
+	bSizer6->Add( m_radioBtn2, 0, wxALL, 5 );
 	
 	
 	bSizer2->Add( bSizer6, 0, wxBOTTOM|wxEXPAND, 5 );
@@ -125,13 +125,13 @@ MainFrameBase::MainFrameBase( wxWindow* parent, wxWindowID id, const wxString& t
 	bSizer7 = new wxBoxSizer( wxHORIZONTAL );
 	
 	m_button1 = new wxButton( m_panel1, wxID_ANY, _("Calcola !!"), wxDefaultPosition, wxSize(dpiz(122), -1), 0 );
-	bSizer7->Add( m_button1, 0, wxLEFT|wxALIGN_LEFT, 5 );
+	bSizer7->Add( m_button1, 0, wxLEFT, 5 );
 	
 	
 	bSizer7->Add( 0, 0, 1, wxEXPAND, 5 );
 	
 	m_button2 = new wxButton( m_panel1, wxID_ANY, _("?"), wxDefaultPosition, wxSize(dpiz(122), -1), 0 );
-	bSizer7->Add( m_button2, 0, wxRIGHT|wxALIGN_RIGHT, 5 );
+	bSizer7->Add( m_button2, 0, wxRIGHT, 5 );
 	
 	
 	bSizer2->Add( bSizer7, 0, wxTOP|wxBOTTOM|wxEXPAND, 5 );
@@ -142,16 +142,16 @@ MainFrameBase::MainFrameBase( wxWindow* parent, wxWindowID id, const wxString& t
 	m_staticText7 = new wxStaticText( m_panel1, wxID_ANY, _("Il tuo peso forma è Kg.:"), wxDefaultPosition, wxSize(dpiz(258), -1), 0 );
 	m_staticText7->Wrap( -1 );
 	m_staticText7->SetFont(wxFontInfo(9).FaceName("Arial"));
-	bSizer8->Add( m_staticText7, 0, wxLEFT|wxALIGN_LEFT, 5 );
+	bSizer8->Add( m_staticText7, 0, wxLEFT, 5 );
 	
-	m_staticText8 = new wxStaticText( m_panel1, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(dpiz(122), -1), 0);
+	m_staticText8 = new wxStaticText( m_panel1, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(dpiz(142), -1), 0);
 	m_staticText8->Wrap( -1 );
 	m_staticText8->SetFont(wxFontInfo(12).FaceName("Arial").Bold());
 	//m_staticText8->SetFont( wxFont( 9, 74, 90, 92, false, wxT("Arial") ) );
     m_staticText8->SetForegroundColour(wxColour(3,7,253));
     m_staticText8->SetBackgroundColour( wxColour( 251, 220, 108 ) );
 	
-	bSizer8->Add( m_staticText8, 0, wxRIGHT|wxALIGN_RIGHT, 5 );
+	bSizer8->Add( m_staticText8, 0, wxRIGHT, 5 );
 	
 	
 	bSizer2->Add( bSizer8, 0, wxEXPAND|wxTOP|wxBOTTOM, 5 );
@@ -163,16 +163,16 @@ MainFrameBase::MainFrameBase( wxWindow* parent, wxWindowID id, const wxString& t
 	m_staticText9->SetFont(wxFontInfo(9).FaceName("Arial").Bold());
 	m_staticText9->Wrap( -1 );
 		
-	bSizer9->Add( m_staticText9, 0, wxLEFT|wxALIGN_LEFT, 5 );
+	bSizer9->Add( m_staticText9, 0, wxLEFT, 5 );
 	
-	m_staticText10 = new wxStaticText( m_panel1, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(dpiz(122), -1), 0);
+	m_staticText10 = new wxStaticText( m_panel1, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(dpiz(142), -1), 0);
 	m_staticText10->Wrap( -1 );
 	m_staticText10->SetFont(wxFontInfo(12).FaceName("Arial").Bold());
 	//m_staticText10->SetFont( wxFont( 9, 74, 90, 92, false, wxT("Arial") ) );
 	m_staticText10->SetForegroundColour( wxColour( 236, 0, 0 ) );
     m_staticText10->SetBackgroundColour( wxColour( 251, 220, 108 ) );
 	
-	bSizer9->Add( m_staticText10, 0, wxRIGHT|wxALIGN_RIGHT, 5 );
+	bSizer9->Add( m_staticText10, 0, wxRIGHT, 5 );
 	
 	
 	bSizer2->Add( bSizer9, 0, wxEXPAND|wxTOP|wxBOTTOM, 5 );
@@ -191,8 +191,8 @@ MainFrameBase::MainFrameBase( wxWindow* parent, wxWindowID id, const wxString& t
 		
 	m_panel1->SetSizer( bSizer2 );
 	m_panel1->Layout();
-	bSizer2->Fit( m_panel1 );
-	bSizer1->Add( m_panel1, 1, wxEXPAND|wxALL|wxALIGN_CENTER_HORIZONTAL, 5 );
+	
+	bSizer1->Add( m_panel1, 1, wxEXPAND|wxALL, 5 );
 		
 	this->SetSizer( bSizer1 );
 	this->Layout();
